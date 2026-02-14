5f5120e178b27ae6c27b168271061759b8e3a79d
Sebastian Bordt <sbordt@posteo.de>
2026-02-14 20:10:57 +0100

    Create pretrain_experiment_4xH100.sh

---
+++ internal/ferranti/pretrain_experiment_4xH100.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --time=3-00:00:00  # Runtime in D-HH:MM:SS    
+#SBATCH --output=/weka/luxburg/sbordt10/logs/pretrain-experiment/%j.out  
+#SBATCH --error=/weka/luxburg/sbordt10/logs/pretrain-experiment/%j.err   
+#SBATCH --open-mode=append
+#SBATCH --job-name=pretrain-experiment  
+#SBATCH --partition=h100-ferranti
+#SBATCH --nodes=1  
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=36    
+#SBATCH --mem=786G   
+#SBATCH --gres=gpu:4              
+
+scontrol show job ${SLURM_JOB_ID}
+nvidia-smi
+
+cd /weka/luxburg/sbordt10/pretrain-experiments
+
+singularity exec --nv \
+  --bind /weka/luxburg/sbordt10:/weka/luxburg/sbordt10 \
+  --env NCCL_TIMEOUT=1800000 \
+  --env TORCH_NCCL_HEARTBEAT_TIMEOUT_SEC=1800 \
+  --env WANDB__SERVICE_WAIT=6000 \
+  --env OLMES_EXECUTABLE=~/venvs/olmes/bin/olmes \
+  pretrain-experiments.sif \
+  python -m pretrain_experiments "$@"
